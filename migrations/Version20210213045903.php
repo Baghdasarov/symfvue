416d38cf7a9f18cc674c342107bb7d6ffad79242
Ruben Iritsyan <treyan94@gmail.com>
2021-02-13 08:59:44 +0400

    Review migration

---
+++ migrations/Version20210213045903.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210213045903 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, hotel_id_id INT NOT NULL, score SMALLINT NOT NULL, comment LONGTEXT NOT NULL, created_date DATETIME NOT NULL, INDEX IDX_794381C69C905093 (hotel_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C69C905093 FOREIGN KEY (hotel_id_id) REFERENCES hotel (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE review');
+    }
+}
